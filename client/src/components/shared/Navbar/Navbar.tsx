cc3cea2e673252e97623bface552acc3b64ffb15
developerMasum <developer.masum2001@gmail.com>
2024-05-03 12:25:40 +0600

    added login and resgister

--- client/src/components/shared/Navbar/Navbar.tsx
+++ client/src/components/shared/Navbar/Navbar.tsx
@@ -1,10 +1,21 @@
-import { Box, Button, Container, Stack, Typography } from "@mui/material";
+"use client"
+import { Box, Container, Stack, Typography } from "@mui/material";
+import dynamic from "next/dynamic";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
+  const AuthButton = dynamic(() => import("@/components/UI/AuthButton/AuthButton"), { ssr: false });
+ 
+
   return (
     <Container>
-      <Stack py={2} direction="row" justifyContent="space-between" alignItems={"center"}>
+      <Stack
+        py={2}
+        direction="row"
+        justifyContent="space-between"
+        alignItems={"center"}
+      >
         <Typography variant="h4" component="h1" fontWeight={600}>
           P
           <Box component="span" color="primary.main">
@@ -12,15 +23,16 @@ const Navbar = () => {
           </Box>
           Health Care
         </Typography>
-        <Stack direction={"row"} gap={4} justifyContent={"space-between"}> 
-
-          <Typography component={Link} href={'/consultation'}>Consultation</Typography>
+        <Stack direction={"row"} gap={4} justifyContent={"space-between"}>
+          <Typography component={Link} href={"/consultation"}>
+            Consultation
+          </Typography>
           <Typography>Health Plans</Typography>
           <Typography>Medicine</Typography>
           <Typography>Diagnostics</Typography>
           <Typography>NGOs</Typography>
         </Stack>
-        <Button component={Link} href="/login">LOGIN</Button>
+       <AuthButton />
       </Stack>
     </Container>
   );
